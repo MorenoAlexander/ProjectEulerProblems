c39977fccc83c2ec277042e023a9907cabf5be1e
MorenoAlexander <37970443+MorenoAlexander@users.noreply.github.com>
2018-06-01 17:19:08 -0500

    Summation of primes
    The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
    
    Find the sum of all the primes below two million.

---
+++ PE10/projectEuler10.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <math.h>
+
+#define MAX 2000000
+using namespace std;
+
+
+
+long long int Esieve_sum(int limit) {
+	int *arr = new int[limit];
+	for (int i = 2; i < limit; i++)
+	{
+		arr[i] = 1;
+	}
+
+	for (int k = 2; k < sqrt(limit); k++) {
+		if ( arr[k] == 1) {
+			for (int j = k*k; j < limit; j = j + k) {
+				arr[j] = 0;
+
+			}
+		}
+	}
+	long long int summation = 0;
+
+	for (int i = 1; i < limit; i++) {
+		if (arr[i] == 1) {
+			summation += i;
+		}
+	}
+
+
+	delete arr;
+	return summation;
+
+
+
+
+
+
+}
+
+
+
+
+int main(void) {
+
+	cout << "Result at 2M: " << Esieve_sum(MAX) << endl;
+
+	system("PAUSE");
+
+	return 0;
+
+}
